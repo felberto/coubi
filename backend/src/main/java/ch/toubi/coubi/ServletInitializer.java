458a2bc26b9abbe50067c56dcb32164ddcdfadb9
felberto <tobias.felber97@gmail.com>
2019-01-12 15:36:52 +0100

    added initial backend

---
+++ backend/src/main/java/ch/toubi/coubi/ServletInitializer.java
@@ -0,0 +1,14 @@
+package ch.toubi.coubi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoubiApplication.class);
+    }
+
+}
+
